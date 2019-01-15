b0f4ae3b7762058dda34f14c3db15d7b301110ef
Marviins <marviins@users.noreply.github.com>
2019-01-15 22:29:05 +0100

    added Readme
    instructions added

---
+++ Bitwarden-backup.ps1
@@ -0,0 +1,31 @@
+﻿#Marviins Bitwarden-backup.ps1
+
+#copy/paste your session key here
+$key = 'sv0Jn8gjYaL1QCyZnmaobZDHEZQ=============================================='
+
+#Specify directory and filenames
+$backupFolder = 'Backup'
+$backupFile = (get-date -Format "yyyyMMdd_hhmmss") + '_Bitwarden-backup.json'
+$attachmentFolder = (get-date -Format "yyyyMMdd_hhmmss") + '_Attachments'
+
+#Backup Vault
+Write-Host "The masterpassword is required to export the vault"
+$masterPass = Read-Host -assecurestring "Please enter your masterpassword"
+$masterPass = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($masterPass))
+
+Write-Host "Exporting Bitwarden Vault"
+.\bw.exe export --output "$backupFolder\$backupFile" --format json --session $key $masterPass
+
+
+#Backup Attachments
+$vault = .\bw.exe list items --session $key | ConvertFrom-Json
+
+foreach ($item in $vault){
+    if($item.PSobject.Properties.Name -contains "Attachments"){
+       foreach ($attachment in $item.attachments){
+           $exportName = '[' + $item.name + '] - ' + $attachment.fileName
+           write-host "Exporting attachments of item: " $item.name
+         .\bw get attachment $attachment.id --itemid $item.id --output "$backupFolder\$attachmentFolder\$exportName" --session $key
+       }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Bitwarden Attachment Export
+
+## Requirements
+Before you can use this Powershell script you'll have to download the official [Bitwarden CLI](https://github.com/bitwarden/cli)
+
+## How to use
+After installing the Bitwarden CLI you can login to your vault using the following commands:
+```
+bw login
+```
+
+If the login is succesful you'll get a session key. Put that session key in the `$key` variable of Bitwarden-backup.ps1 and start the script.
