8f3c852e331a454e2fa7c6d6c199bf0d26887ec3
Justin Wong <32123388+justincswong@users.noreply.github.com>
2020-01-11 17:03:12 -0800

    Updated Bitwarden-backup.ps1
    -Master password is only required once from twice
    -Automatically asks for the password again if previous tries were incorrect

--- Bitwarden-backup.ps1
+++ Bitwarden-backup.ps1
@@ -1,34 +1,54 @@
 ﻿#	Bitwarden-Attachment-Exporter
-#	Marviins
+#	Marviins, edited by justinc.s.wong
 
-#copy/paste your session key here
-$key = 'sv0Jn8gjYaL1QCyZnmaobZDHEZQ=============================================='
+    # Enter Master Password
+    $username   = "REPLACE USERNAME HERE" #keep the quotes
+    $masterPass = Read-Host -assecurestring "Please enter your master password"
+    $masterPass = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($masterPass))
+    $key = $null
+    
 
-#Specify directory and filenames
-$backupFolder = 'Backup'
-$backupFile = (get-date -Format "yyyyMMdd_hhmmss") + '_Bitwarden-backup.json'
-$attachmentFolder = (get-date -Format "yyyyMMdd_hhmmss") + '_Attachments'
-
-#Backup Vault
-Write-Host "The masterpassword is required to export the vault"
-$masterPass = Read-Host -assecurestring "Please enter your masterpassword"
-$masterPass = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($masterPass))
-
-Write-Host "`nExporting Bitwarden Vault"
-.\bw.exe export --output "$backupFolder\$backupFile" --format json --session $key $masterPass
-write-host "`n"
-
-#Backup Attachments
-$vault = .\bw.exe list items --session $key | ConvertFrom-Json
-
-foreach ($item in $vault){
-    if($item.PSobject.Properties.Name -contains "Attachments"){
-       foreach ($attachment in $item.attachments){
-           $exportName = '[' + $item.name + '] - ' + $attachment.fileName
-         .\bw.exe get attachment $attachment.id --itemid $item.id --output "$backupFolder\$attachmentFolder\$exportName" --session $key
-		write-host "`n"
-	   }
+while ($key -eq $null) {
+    try {
+        $key = bw login $username $masterPass --raw
+        "`n"
+        if ($key -eq $null) {
+            throw "Invalid Password Exception"
+        }
+    }
+    catch {
+        $masterPass = Read-Host -assecurestring "`nPlease re-enter your master password"
+        $masterPass = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($masterPass))
     }
 }
 
-write-host "`nWe're finished!"
+Write-Host "You have logged in." -ForegroundColor Green
+$env:BW_SESSION="$key"
+
+#Specify directory and filenames
+$backupFolder = 'Backup'
+$backupFile = (get-date -Format "yyyyMMdd_hhmmss") + '_Bitwarden_backup.csv'
+$attachmentFolder = (get-date -Format "yyyyMMdd_hhmmss") + '_Attachments'
+
+    #Backup Vault
+    Write-Host "`nExporting Bitwarden Vault"
+    bw export --output "$backupFolder\$backupFile" --format csv $masterPass
+    write-host "`n"
+
+    #Backup Attachments
+    $vault = bw list items | ConvertFrom-Json
+
+    foreach ($item in $vault){
+        if($item.PSobject.Properties.Name -contains "Attachments"){
+           foreach ($attachment in $item.attachments){
+              $exportName = '[' + $item.name + '] - ' + $attachment.fileName
+            bw get attachment $attachment.id --itemid $item.id --output "$backupFolder\$attachmentFolder\$exportName"
+	    	write-host "`n"
+	     }
+      }
+    }
+
+    write-host "`nThe Vault has been successfully backed up."
+    bw logout
+    "`n"
+    pause
